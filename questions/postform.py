c9b9efaae8d666ccf2a06a405931459b253cd21c
Thibault DUPONCHELLE <thibault.duponchelle@amadeus.net>
2020-01-14 11:04:31 +0100

    Profile, users, fixes, css

--- questions/postform.py
+++ questions/postform.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class QuestionForm(forms.Form):
-  title = forms.CharField(label="",min_length=300, max_length=300, widget=forms.TextInput(attrs={'size' : '100'}))
+  title = forms.CharField(label="", max_length=300, widget=forms.TextInput(attrs={'size' : '100'}))
   content = forms.CharField(label="", widget=forms.Textarea(attrs={'id' : 'markdown'}))
 
 class AnswerForm(forms.Form):
